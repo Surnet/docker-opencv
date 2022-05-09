9b473b3d13e33e08d67741449ed6efcef0c41c4d
Daniel Müller <daniel.mueller@surnet.ch>
2022-05-09 15:25:48 +0200

    Add text from contrib (#1)
    * Add param
    
    * Add edtions
    
    * Fix lib dir
    
    * Cleanup build files
    
    * new version
    
    Co-authored-by: Reto Arnold <reto.arnold@surnet.ch>

--- update.sh
+++ update.sh
@@ -24,68 +24,76 @@ function docker_tag_exists() {
 
 # opencv versions
 for version in \
-  4.5.1 \
+  4.5.4 \
 ; do
-
-  # Supported base images
-  for image in \
-    alpine:3.15.0 \
-    node:16.13.0-alpine3.13 \
+  
+  # edition small (opencv only) or full (with opencv_contrib)
+  for edition in \
+    base \
+    text \
   ; do
-    # Parse image string
-    base="${image%%:*}"
-    baseVersion="${image##*:}"
-    baseVersionClean="${baseVersion%%-*}"
 
-    # Check for base OS type (currently only alpine)
-    case "$image" in
-      alpine*)
-        os="alpine"
-        template="Dockerfile-alpine.template"
-        replaceRules="
-          s/%%IMAGE%%/$image/g;
-          s/%%OPENCVVERSION%%/$version/g;
-          /%%END%%/d;
-        "
-      ;;
-      *alpine*)
-        os="alpine"
-        template="Dockerfile-alpine.template"
-        replaceRules="
-          s/%%IMAGE%%/$image/g;
-          s/%%OPENCVVERSION%%/$version/g;
-          s/%%END%%/ENV OPENCV4NODEJS_DISABLE_AUTOBUILD=1/g;
-        "
-      ;;
-      *)
-        echo "WARNING: OS Type not supported"
-        exit
-      ;;
-    esac
+    # Supported base images
+    for image in \
+      alpine:3.15.4 \
+      node:16.14.2-alpine3.15 \
+    ; do
+      # Parse image string
+      base="${image%%:*}"
+      baseVersion="${image##*:}"
+      baseVersionClean="${baseVersion%%-*}"
 
-    # Prepare imageName and tag
-    if [ "$os" == "$base" ]; then
-      imageName="$base-opencv"
-    else
-      imageName="$os-$base-opencv"
-    fi
-    tag="$baseVersionClean-$version"
-    dir="archive/$imageName"
-    file="Dockerfile_$tag"
+      # Check for base OS type (currently only alpine)
+      case "$image" in
+        alpine*)
+          os="alpine"
+          template="Dockerfile-alpine-$edition.template"
+          replaceRules="
+            s/%%IMAGE%%/$image/g;
+            s/%%OPENCVVERSION%%/$version/g;
+            /%%END%%/d;
+          "
+        ;;
+        *alpine*)
+          os="alpine"
+          template="Dockerfile-alpine-$edition.template"
+          replaceRules="
+            s/%%IMAGE%%/$image/g;
+            s/%%OPENCVVERSION%%/$version/g;
+            s/%%END%%/ENV OPENCV4NODEJS_DISABLE_AUTOBUILD=1/g;
+          "
+        ;;
+        *)
+          echo "WARNING: OS Type not supported"
+          exit
+        ;;
+      esac
 
-    # Build container if needed
-    if ! docker_tag_exists "surnet/$imageName" "$tag"; then
-      # Prepare Dockerfile
-      mkdir -p "$dir"
-      { generated_warning; cat "$template"; } > "$dir/$file"
-      sed -i.bak -e "$replaceRules" "$dir/$file"
+      # Prepare imageName and tag
+      if [ "$os" == "$base" ]; then
+        imageName="$base-opencv"
+      else
+        imageName="$os-$base-opencv"
+      fi
+      tag="$baseVersionClean-$version-$edition"
+      dir="archive/$imageName"
+      file="Dockerfile_$tag"
 
-      # Build container
-      echo "Starting build for surnet/$imageName:$tag"
-      docker buildx build . -f "$dir/$file" -t "surnet/$imageName:$tag" --platform linux/amd64,linux/arm64 --push \
-      && docker buildx build . -f "$dir/$file" -t "ghcr.io/surnet/$imageName:$tag" --platform linux/amd64,linux/arm64 --push \
-      && echo "Successfully built and pushed surnet/$imageName:$tag" || echo "Building or pushing failed for surnet/$imageName:$tag"
-    fi
+      # Build container if needed
+      if ! docker_tag_exists "surnet/$imageName" "$tag"; then
+        # Prepare Dockerfile
+        mkdir -p "$dir"
+        { generated_warning; cat "$template"; } > "$dir/$file"
+        sed -i.bak -e "$replaceRules" "$dir/$file"
+
+        # Build container
+        echo "Starting build for surnet/$imageName:$tag"
+        docker buildx build . -f "$dir/$file" -t "surnet/$imageName:$tag" --platform linux/amd64,linux/arm64 --push \
+        && docker buildx build . -f "$dir/$file" -t "ghcr.io/surnet/$imageName:$tag" --platform linux/amd64,linux/arm64 --push \
+        && echo "Successfully built and pushed surnet/$imageName:$tag" || echo "Building or pushing failed for surnet/$imageName:$tag"
+      fi
+      
+    done
 
   done
 
